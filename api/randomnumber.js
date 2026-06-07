ebedf9db7e7f8a934fe649fa68f0e57712b640bc
Marco Schmidt <marcoschmidt15@googlemail.com>
2026-06-07 15:46:39 +0200

    Update randomnumber.js

--- api/randomnumber.js
+++ api/randomnumber.js
@@ -2,6 +2,8 @@ export default function handler(req, res) {
   const number = Math.floor(Math.random() * 100) + 1;
 
   res.status(200).json({
-    number
+    number,
+    timestamp: new Date().toISOString(),
+    source: "https://interfaces.marco-schmidt.com/randomnumber
   });
 }
