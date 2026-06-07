03ca60b73d12ee39caee58ab776e5c173131992c
Marco Schmidt <marcoschmidt15@googlemail.com>
2026-06-07 15:46:14 +0200

    Add timestamp and source to random color response

--- api/randomcolor.js
+++ api/randomcolor.js
@@ -4,5 +4,7 @@ export default function handler(req, res) {
 
   res.status(200).json({
     color
+    timestamp: new Date().toISOString(),
+    source: "https://interfaces.marco-schmidt.com/randomcolor
   });
 }
